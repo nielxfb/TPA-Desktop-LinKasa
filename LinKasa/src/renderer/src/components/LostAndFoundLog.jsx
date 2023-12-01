c148c177eb35fb652bb9f275ea99ffeacd95266c
unknown <adamludnl@gmail.com>
2023-12-01 07:55:51 +0700

    Finished Update Item Status in Lost and Found Log

--- LinKasa/src/renderer/src/components/LostAndFoundLog.jsx
+++ LinKasa/src/renderer/src/components/LostAndFoundLog.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Utils from '../controller/Utils';
-import { collection, deleteDoc, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, getDocs, updateDoc } from 'firebase/firestore';
 import { db, storage } from '../../../../firebase.config';
 import { Link, Outlet } from 'react-router-dom';
 import { deleteObject, ref } from 'firebase/storage';
+import ReactModal from 'react-modal';
 
 const LostAndFoundLog = () => {
   const authorized = Utils.useRoleCheck('Lost and Found Staff');
   const [lostItems, setLostItems] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedItemName, setSelectedItemName] = useState('');
+  const [selectedStatus, setSelectedStatus] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchLostItems = async () => {
@@ -25,6 +30,42 @@ const LostAndFoundLog = () => {
 
     fetchLostItems();
   }, [authorized]);
+
+  const useOpenModal = (itemName) => {
+    setSelectedItemName(itemName);
+    setIsModalOpen(true);
+  }
+
+  const handleStatusChange = (status) => {
+    if(selectedStatus === status) setSelectedStatus('');
+    else if(selectedStatus != status) setSelectedStatus(status);
+  }
+
+  const handleEditStatus = async () => {
+    if(selectedStatus === ''){
+      setError('Please set item status!');
+      setIsModalOpen(false);
+      return;
+    } else {
+      setError('');
+    }
+
+    const q = Utils.queryFromCollection('items', 'name', selectedItemName);
+    const querySnapshot = await getDocs(q);
+    let docRef;
+    querySnapshot.forEach((doc) => {
+      docRef = doc.ref;
+    });
+
+    if(docRef){
+      await updateDoc(docRef, {
+        status: selectedStatus
+      });
+
+      alert('Successfully updated item status!');
+      window.location.reload();
+    }
+  }
 
   const handleRemoveItem = async (itemName) => {
     const q = Utils.queryFromCollection('items', 'name', itemName);
@@ -81,7 +122,12 @@ const LostAndFoundLog = () => {
                 <td className='border px-4 py-2'>{item.status}</td>
                 <td className='text-center'>
                   <div className='inline-flex justify-center p-1'>
-                    <button className='bg-green-500 text-white py-1 px-2 rounded-md mr-2'>Edit</button>
+                    <button
+                      className='bg-green-500 text-white py-1 px-2 rounded-md mr-2'
+                      onClick={() => useOpenModal(item.name)}
+                    >
+                      Edit
+                    </button>
                     <button
                       className='bg-red-500 text-white py-1 px-2 rounded-md'
                       onClick={() => handleRemoveItem(item.name)}
@@ -95,12 +141,41 @@ const LostAndFoundLog = () => {
           </tbody>
         </table>
       )}
+      {error && (
+        <div className="text-red-500 mb-4">
+          <p>{error}</p>
+        </div>
+      )}
       <Link
         to='/lost-and-found-log/update'
         className='mt-2 px-4 py-2 text-white bg-slate-500 rounded-md shadow-lg'
       >
         Update Lost and Found Log
       </Link>
+      <ReactModal
+        isOpen={isModalOpen}
+        onRequestClose={handleEditStatus}
+        contentLabel='Status Modal'
+      >
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <h1 className='text-xl font-bold'>Please set the status</h1>
+          <div className='flex flex-row gap-2 mt-4'>
+            <button
+              onClick={() => handleStatusChange('Unclaimed')}
+              className={`text-white px-4 py-2 rounded-md ${selectedStatus === 'Unclaimed' ? 'bg-red-500' : 'bg-gray-500'}`}
+            >
+              Unclaimed
+            </button>
+            <button
+              onClick={() => handleStatusChange('Returned to Owner')}
+              className={`text-white px-4 py-2 rounded-md ${selectedStatus === 'Returned to Owner' ? 'bg-green-500' : 'bg-gray-500'}`}
+            >
+              Returned to Owner
+            </button>
+          </div>
+          <button onClick={handleEditStatus} className='bg-blue-500 text-white px-4 py-2 rounded-md mt-4'>Close</button>
+        </div>
+      </ReactModal>
       <Outlet />
     </div>
   )
