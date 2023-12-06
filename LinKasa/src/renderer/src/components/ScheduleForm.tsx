ef6c0da8d961d1b134ba782d40fe0e1c8335d7a4
Daniel Adamlu <113871018+nielxfb@users.noreply.github.com>
2023-12-07 00:07:53 +0700

    Added upload new flight schedule

--- LinKasa/src/renderer/src/components/ScheduleForm.tsx
+++ LinKasa/src/renderer/src/components/ScheduleForm.tsx
@@ -1,11 +1,15 @@
+import { handleSubmitForm } from '@renderer/controller/ScheduleController';
 import { useRoleCheck } from '@renderer/controller/Utils';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function ScheduleForm(): JSX.Element {
   const authorized = useRoleCheck(['Flight Operations Manager', 'COO']);
+  const [error, setError] = useState<string>('');
 
   const airlineNameRef = useRef<HTMLSelectElement>(null);
   const flightNumberRef = useRef<HTMLInputElement>(null);
+  const destinationRef = useRef<HTMLInputElement>(null);
+  const departureTimeRef = useRef<HTMLInputElement>(null);
 
   if (!authorized) {
     return (
@@ -18,27 +22,51 @@ function ScheduleForm(): JSX.Element {
   return (
     <div className="max-w-md mx-auto mt-8 p-8 bg-white shadow-lg rounded-md">
       <h2 className="text-2xl font-semibold mb-4">Create Flight Schedule</h2>
-      <form onSubmit={() => {}}>
+      <form onSubmit={(e) => handleSubmitForm(e, airlineNameRef, flightNumberRef, destinationRef, departureTimeRef, setError)}>
         <div className="mb-4">
           <label htmlFor="airline-name" className="block text-sm font-medium text-gray-600">
             Airline Name
           </label>
-          <select ref={airlineNameRef}>
-            <option value="Garuda Indonesia">Garuda Indonesia</option>
-            <option value="Air Asia">Air Asia</option>
+          <select ref={airlineNameRef} className="mt-1 p-2 w-full border rounded-md">
+            <option value="Garuda_Indonesia">Garuda Indonesia</option>
+            <option value="Batik_Air">Batik Air</option>
+            <option value="Air_Asia">Air Asia</option>
             <option value="Citilink">Citilink</option>
-            <option value="Super Air Jet">Super Air Jet</option>
-            <option value="Garuda Indonesia">Garuda Indonesia</option>
-            <option value="Garuda Indonesia">Garuda Indonesia</option>
+            <option value="Super_Air_Jet">Super Air Jet</option>
+            <option value="Singapore_Airline">Singapore Airlines</option>
+            <option value="Cathay_Pacific">Cathay Pacific</option>
           </select>
         </div>
         <div className="mb-4">
           <label htmlFor="flight-number" className="block text-sm font-medium text-gray-600">
             Flight Number
           </label>
-          <input type="text" ref={flightNumberRef} />
+          <input type="text" ref={flightNumberRef} className="mt-1 p-2 w-full border rounded-md" />
         </div>
+        <div className="mb-4">
+          <label htmlFor="destination" className="block text-sm font-medium text-gray-600">
+            Destination
+          </label>
+          <input type="text" ref={destinationRef} className="mt-1 p-2 w-full border rounded-md" />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="departure-time" className="block text-sm font-medium text-gray-600">
+            Departure Time
+          </label>
+          <input type="datetime-local" ref={departureTimeRef} className="mt-1 p-2 w-full border rounded-md" />
+        </div>
+        <button
+          type="submit"
+          className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600"
+        >
+          Submit Item
+        </button>
       </form>
+      {error && (
+        <div className="text-red-500 mb-4">
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 }
--- LinKasa/src/renderer/src/controller/ScheduleController.tsx
+++ LinKasa/src/renderer/src/controller/ScheduleController.tsx
@@ -1,6 +1,9 @@
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../../../firebase/firebase';
+import { Timestamp, addDoc, collection, getDocs } from 'firebase/firestore';
+import { db, storage } from '../../../firebase/firebase';
 import { Schedule } from '@renderer/model/Schedule';
+import { queryFromCollection } from './Utils';
+import { getDownloadURL, ref } from 'firebase/storage';
+import { useState } from 'react';
 
 const fetchSchedules = async (): Promise<Schedule[]> => {
   const q = collection(db, 'departures');
@@ -14,4 +17,47 @@ const fetchSchedules = async (): Promise<Schedule[]> => {
   return departures;
 };
 
-export { fetchSchedules };
+const checkUniqueFlightNumber = async (flightNumber: string): Promise<boolean> => {
+  const querySnapshot = await getDocs(
+    queryFromCollection('departures', 'flight_number', flightNumber)
+  );
+  return querySnapshot.empty;
+};
+
+const handleSubmitForm = async (
+  e: React.FormEvent<HTMLFormElement>,
+  airlineNameRef: React.RefObject<HTMLSelectElement>,
+  flightNumberRef: React.RefObject<HTMLInputElement>,
+  destinationRef: React.RefObject<HTMLInputElement>,
+  departureTimeRef: React.RefObject<HTMLInputElement>,
+  setError: React.Dispatch<React.SetStateAction<string>>
+): Promise<void> => {
+  e.preventDefault();
+
+  const airlineName: string = airlineNameRef.current?.value || '';
+  const flightNumber: string = flightNumberRef.current?.value || '';
+  const destination: string = destinationRef.current?.value || '';
+  const departureTime: string = departureTimeRef.current?.value || '';
+
+  if (flightNumber && !(await checkUniqueFlightNumber(flightNumber))) {
+    setError('Flight number already exists!');
+    return;
+  }
+
+  const storageRef = ref(storage, 'airline_image/' + airlineName + '.png');
+
+  getDownloadURL(storageRef).then((url) => {
+    addDoc(collection(db, 'departures'), {
+      airline_path: url,
+      departure_time: Timestamp.fromDate(new Date(departureTime)),
+      destination: destination,
+      flight_number: flightNumber
+    }).then(() => {
+      alert('Successfully uploaded new flight schedule!');
+      window.location.reload();
+    });
+  });
+
+};
+
+export { fetchSchedules, handleSubmitForm };
